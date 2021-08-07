349052f5adbbf50bc1a1708ce3abedfe8f737eeb
João <joaovictorocosta@gmail.com>
2021-08-07 16:56:32 -0300

    Header's Top, Title and Logo styled components created. Logo added to App

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -4,3 +4,18 @@ export const Container = styled.View`
   align-items: center;
   padding: 40px 0px 30px;
 `;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const Logo = styled.Image``;
+
+export const Title = styled.Text`
+  font-size: 10px;
+  font-weight: bold;
+  color: #fff;
+  margin-left: 8px;
+`;
